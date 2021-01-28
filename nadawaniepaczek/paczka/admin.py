110a2144eae5454e3719eab2aef1e76ee849f4ae
dhtgaming <47740790+dhtgaming@users.noreply.github.com>
2021-01-28 03:02:22 +0100

    Update admin.py

--- nadawaniepaczek/paczka/admin.py
+++ nadawaniepaczek/paczka/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Dostawca, Nadawca, Odbiorca, Paczka, Harmonogrampracy, Zamowienie
 
-admin.site.register(admin)
+admin.site.register(Dostawca)
+admin.site.register(Nadawca)
+admin.site.register(Odbiorca)
+admin.site.register(Paczka)
+admin.site.register(Harmonogrampracy)
+admin.site.register(Zamowienie)
